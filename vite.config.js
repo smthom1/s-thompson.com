1ffa3aecd4d321f585bacea197e2a052e7573e9d
Sophia Thompson <119273210+smthom1@users.noreply.github.com>
2025-11-09 13:13:17 -0800

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/sophia-thompson/", // 👈 Important for GitHub Pages
-});
